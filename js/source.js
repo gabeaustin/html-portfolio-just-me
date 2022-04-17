ea4f4cff10d140ed5d260a03b3999a7f57a3e5e0
Gabriel Austin <gabriel.austin2019@gmail.comt>
2022-04-16 22:48:32 -0500

    added js - working on mouseover event for resume download btn

---
+++ js/source.js
@@ -0,0 +1,10 @@
+// let fillBackground = document.getElementById("download-resume-btn");
+
+// fillBackground.addEventListener("mouseover", function (event) {
+//   // event.target.style.color = "hotpink";
+//   console.log("mouseover works");
+// });
+
+function fillBackground() {
+  document.getElementById("download-resume-btn").style.backgroundColor = "hotpink";
+}
